b0d9a24cae8408795e611e68bc9559c7d0e4c1d5
neelabh2010 <neelabh2010@gmail.com>
2021-02-09 14:46:01 +0530

    FTPSet

---
+++ NeelabhCoreTools/Sets/FTPSet.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NeelabhCoreTools.Sets
+{
+
+    [Owned]
+    public class FTPSet
+    {
+        [Column(TypeName = "varchar(50)")]
+        public string Host { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string User { get; set; }
+
+        [Column(TypeName = "varchar(400)")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Root { get; set; }
+    }
+}
